effff00a9c6d69d090442a4e41e83827332c32c4
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-07-02 19:11:30 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Component } from 'react'
-import Table from './react/Table'
+import Table from './components/Table'
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <Table />
-            </div>
+            <Table />
         )
     }
 }
